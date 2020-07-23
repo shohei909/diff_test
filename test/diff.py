7099d5bf64854acfcefe82f7a3060be8ca05eac7
shohei909 <909mmsphere@gmail.com>
2020-07-23 19:35:51 +0900

    add test scripts

---
+++ test/diff.py
@@ -0,0 +1,8 @@
+
+import subprocess
+import os
+
+# Git で差分を表示
+test_dir = os.path.dirname(__file__)
+subprocess.run(["git", "add", "-N", test_dir + "/out"])
+subprocess.run(["git", "--no-pager", "diff", "--relative=test/out", "-b"])
---
+++ test/run.py
@@ -0,0 +1,25 @@
+import glob
+import subprocess
+import os
+import sys
+import shutil
+
+test_dir = os.path.dirname(__file__)
+
+# 出力ディレクトリのクリーン
+if os.path.isdir(test_dir + "/out"):
+    shutil.rmtree(test_dir + "/out")
+
+os.makedirs(test_dir + "/out", exist_ok=True)
+
+for file in glob.glob(test_dir + "/code/*.py"):
+    file_name = os.path.splitext(os.path.basename(file))[0]
+
+    extension = ".txt"
+
+    out_file_path = test_dir + "/out/" + file_name + extension
+    out_file = open(out_file_path, "w")
+
+    # テストを実行
+    python_executable = sys.executable
+    subprocess.run([python_executable, file], stdout=out_file)
