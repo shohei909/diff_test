46c08167fb95ae9551805e240627c3d6eea678cb
shohei909 <909mmsphere@gmail.com>
2020-07-24 07:34:25 +0900

    improve run.py

--- test/run.py
+++ test/run.py
@@ -2,18 +2,14 @@ import glob
 import subprocess
 import os
 import sys
-import shutil
 
 test_dir = os.path.dirname(__file__)
+out_files = set()
 
-# 出力ディレクトリのクリーン
-if os.path.isdir(test_dir + "/out"):
-    shutil.rmtree(test_dir + "/out")
-
-os.makedirs(test_dir + "/out", exist_ok=True)
-
-for file in glob.glob(test_dir + "/code/*.py"):
-    file_name = os.path.splitext(os.path.basename(file))[0]
+# code 以下の *.py ファイルを検索
+for file in glob.glob(test_dir + "/code/**/*.py", recursive=True):
+    relpath = os.path.relpath(file, test_dir + "/code")
+    file_name = os.path.splitext(relpath)[0]
 
     extension = ".txt"
 
@@ -21,8 +17,18 @@ for file in glob.glob(test_dir + "/code/*.py"):
         extension = ".svg"
 
     out_file_path = test_dir + "/out/" + file_name + extension
+    
+    os.makedirs(os.path.dirname(out_file_path), exist_ok=True) # 出力フォルダが無ければ作成
     out_file = open(out_file_path, "w")
 
-    # テストを実行
-    python_executable = sys.executable
-    subprocess.run([python_executable, file], stdout=out_file)
+    # `python $test.py` のプロセスを実行
+    subprocess.run([sys.executable, file], stdout=out_file)
+    
+    # 出力したファイルを記録
+    out_files.add(os.path.abspath(out_file_path))
+
+# 削除ずみのテストの出力を削除
+for file in glob.glob(test_dir + "/out/**/*", recursive=True):
+    if os.path.isfile(file):
+        if not os.path.abspath(file) in out_files: # 出力ファイルに含まれてないファイルを削除
+            os.remove(file)
