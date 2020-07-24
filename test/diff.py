55c582d6b0df6ef3975cf91df6739a061546c21c
shohei909 <909mmsphere@gmail.com>
2020-07-24 20:05:39 +0900

    improve command

--- test/diff.py
+++ test/diff.py
@@ -5,4 +5,4 @@ import os
 # Git で差分を表示
 test_dir = os.path.dirname(__file__)
 subprocess.run(["git", "add", "-N", test_dir + "/out"])
-subprocess.run(["git", "--no-pager", "diff", "--relative=test/out", "-b"])
+subprocess.run(["git", "--no-pager", "diff", "--relative=test/out", "--ignore-space-change"])
